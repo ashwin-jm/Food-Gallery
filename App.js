8b3a19bac419a22ad5a78324ee0a33c969360b69
Ashwin Joseph Manthara <73656556+ashwin-jm@users.noreply.github.com>
2022-08-07 22:22:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import TinderCards from './component/TinderCards';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TinderCards>
+      </TinderCards>
+      <Footer />
+    </div>
+    
+  );
+}
+
+export default App;
